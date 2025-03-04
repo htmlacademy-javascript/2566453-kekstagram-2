e9f7b75c8e3a65316f916169829a66b026b6ea25
Андрей Кузин <Ziikes@ya.ru>
2025-03-04 13:47:20 +0500

    Правит конкатенацию

--- js/main.js
+++ js/main.js
@@ -31,17 +31,17 @@ const ObjectGenerator = function(){
   for(let i = 0; i < 25; i++){
     const user = new Object({
       id: i + 1,
-      url: 'photos/' + (i + 1) + '.jpg',
+      url: `photos/${i + 1}.jpg`,
       description: 'Момент, запечатленный во времени, отражающий красоту и уникальность мира вокруг нас, где каждый элемент и деталь рассказывают свою собственную историю и вызывают эмоции, которые остаются с нами навсегда.',
       likes: CountLike(),
       comments: {
         id: CommetIdValue(),
-        avatar: 'img/avatar-' + IdValue() + '.svg',
+        avatar: `img/avatar-${IdValue()}.svg`,
         message: MessageValue(),
         name: NameValue()
       }
     });
     base.push(user);
   }
-}
+};
 ObjectGenerator();
